11661a3a35ba9dfb0e0561c45c212133ad2c3ab4
Eduardo B. Alexandre <sezdocs@github.com>
2024-04-20 18:26:37 -0300

    fix: Fix typespec

--- lib/flashy.ex
+++ lib/flashy.ex
@@ -1,15 +1,13 @@
 defmodule Flashy do
   @moduledoc false
 
-  alias Flashy.Notification
-
   alias Phoenix.{Controller, LiveView, LiveView.Socket}
 
   alias Plug.Conn
 
-  @spec put_notification(Conn.t() | Socket.t(), Notification.Protocol.t()) :: Conn.t() | Socket.t()
   def put_notification(%Socket{} = socket, notification) do
     key = :erlang.unique_integer([:positive, :monotonic]) |> to_string() |> then(&"flashy-#{&1}")
+  @spec put_notification(Conn.t() | Socket.t(), Flashy.Protocol.t()) :: Conn.t() | Socket.t()
 
     LiveView.put_flash(socket, key, notification)
   end
@@ -20,9 +18,9 @@ defmodule Flashy do
     Controller.put_flash(conn, key, notification)
   end
 
-  @spec send_notification(Socket.t(), Notification.Protocol.t(), pid) :: Socket.t()
   def send_notification(%Socket{} = socket, notification, pid \\ self()) do
     send(pid, {:flashy_notification, notification})
+  @spec send_notification(Socket.t(), Flashy.Protocol.t(), pid) :: Socket.t()
 
     socket
   end
